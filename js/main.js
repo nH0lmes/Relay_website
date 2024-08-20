60c6b5c2716eb74ffe5b0ea647e52a0bc3663143
Nathan Holmes <nathanph2004@gmail.com>
2024-08-20 22:30:31 +0100

    Fully fixed and completed form section

--- js/main.js
+++ js/main.js
@@ -1,72 +1,73 @@
 let nameCount = 4;
 
-document.getElementById('addSwimmerButton').addEventListener('click',function(){
+document
+  .getElementById("addSwimmerButton")
+  .addEventListener("click", function () {
     nameCount++;
+    const wrapper = document.createElement("div");
+    wrapper.className = "wrapper";
 
-    const nameBox = document.createElement('div');
-    nameBox.className = 'swimmer-input';
-    nameBox.dataset.index  =nameCount;
+    const nameBox = document.createElement("div");
+    nameBox.className = "swimmer-input new-one";
+    nameBox.dataset.index = nameCount;
 
-    const label = document.createElement('label');
-    label.setAttribute('for', `Swimmer${nameCount}`);
+    const label = document.createElement("label");
+    label.setAttribute("for", `Swimmer${nameCount}`);
     label.textContent = `Swimmer ${nameCount}`;
 
-    const divider = document.createElement('div');
-    divider.className = 'divider';
+    const divider = document.createElement("div");
+    divider.className = "divider";
 
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.inputMode = 'numeric';
+    const input = document.createElement("input");
+    input.type = "text";
+    input.inputMode = "numeric";
     input.id = `Swimmer${nameCount}`;
     input.name = `Swimmer${nameCount}`;
 
-    const deleteButtonContainer = document.createElement('div');
-    deleteButtonContainer.className = 'delete-button-container';
+    const deleteButton = document.createElement("button");
+    deleteButton.type = "button";
+    deleteButton.textContent = "-";
+    deleteButton.className = "btn-delete";
 
-    const deleteButton = document.createElement('button');
-    deleteButton.type = 'button';
-    deleteButton.textContent = '-';
-    deleteButton.className = 'btn-delete';
-
-    deleteButton.addEventListener('click',function(){
-        nameBox.remove();
-        deleteButtonContainer.remove();
-        reindexSwimmerBoxes();
+    deleteButton.addEventListener("click", function () {
+      wrapper.remove();
+      reindexSwimmerBoxes();
     });
-    deleteButtonContainer.appendChild(deleteButton);
 
     nameBox.appendChild(label);
     nameBox.appendChild(divider);
     nameBox.appendChild(input);
 
+    wrapper.appendChild(nameBox);
+    wrapper.appendChild(deleteButton);
 
-    const form = document.getElementById('swimmer-form');
-    form.insertBefore(nameBox, document.getElementById('addSwimmerButton'));
-    form.insertBefore(deleteButtonContainer,document.getElementById('addSwimmerButton'));
-    form.insertBefore(deleteButtonContainer, nameBox.nextSibling);
-}); 
+    const form = document.getElementById("swimmer-form");
+    form.insertBefore(wrapper, document.getElementById("buttons-container"));
+  });
 
-function reindexSwimmerBoxes(){
-    const swimmerBoxes = document.querySelectorAll('.swimmer-input');
-    const deleteButtonsContainers = document.querySelectorAll('delete-button-container');
-    
-    nameCount = swimmerBoxes.length;
+function reindexSwimmerBoxes() {
+  const swimmerBoxes = document.querySelectorAll(".swimmer-input");
+  const deleteButtonsContainers = document.querySelectorAll(
+    "delete-button-container"
+  );
 
-    swimmerBoxes.forEach((box, index)=>{
-        const newIndex = index + 1;
-        box.dataset.index = newIndex;
+  nameCount = swimmerBoxes.length;
 
-        const label =box.querySelector('label');
-        const input = box.querySelector('input');
+  swimmerBoxes.forEach((box, index) => {
+    const newIndex = index + 1;
+    box.dataset.index = newIndex;
 
-        label.setAttribute('for', `Swimmer${newIndex}`);
-        label.textContent = `Swimmer ${newIndex}`;
-        input.id = `Swimmer${newIndex}`;
-        input.name = `Swimmer${newIndex}`;
-    });
+    const label = box.querySelector("label");
+    const input = box.querySelector("input");
 
-    deleteButtonsContainers.forEach((container,index)=>{
+    label.setAttribute("for", `Swimmer${newIndex}`);
+    label.textContent = `Swimmer ${newIndex}`;
+    input.id = `Swimmer${newIndex}`;
+    input.name = `Swimmer${newIndex}`;
+  });
+
+  deleteButtonsContainers.forEach((container, index) => {
     const newIndex = index + 1;
     container.dataset.index = newIndex;
-    });
+  });
 }
